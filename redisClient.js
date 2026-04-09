e2d262ca576b6f9560cd5e889db24c7555530ffe
Teja2698 <bteja2698@gmail.com>
2026-04-09 11:41:16 -0500

    Add Redis URL debug log

--- redisClient.js
+++ redisClient.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 const redis = require("redis");
 
+const maskedRedisUrl = process.env.REDIS_URL
+    ? process.env.REDIS_URL.replace(/:(?!\/\/)[^:@]+@/, ":***@")
+    : "REDIS_URL not set";
+
+console.log("Using REDIS_URL:", maskedRedisUrl);
+
 const client = redis.createClient({
     url: process.env.REDIS_URL
 });
